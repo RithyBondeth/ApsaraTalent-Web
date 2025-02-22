0ae38dac9ca6dd88a8e44f05448d4a9ea90803af
Rithy Bondeth <rithybondeth999@gmail.com>
2025-02-22 10:08:42 +0700

    handle routing with left-sidebar menu

---
+++ app/(main)/matching/page.tsx
@@ -0,0 +1,7 @@
+export default function MatchingPage() {
+    return (
+        <div>
+            Matching Page
+        </div>
+    )
+}
--- components/sidebar/left-sidebar/index.tsx
+++ components/sidebar/left-sidebar/index.tsx
@@ -5,12 +5,15 @@ import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { TypographyP } from "@/components/utils/typography/typography-p";
 import DropdownMore from "./dropdown-more";
+import Link from "next/link";
 
 export default function Sidebar({className}: {className?: string}) {
     return (
         <div className={cn("sticky top-5 flex flex-col items-start justify-between p-5 border border-r-muted", className)}>
             <div className="w-full">
-                <LogoComponent/>
+                <Link href="/feed">
+                    <LogoComponent/>
+                </Link>
                 <div className="flex flex-col gap-3 mt-8">
                     <MenuItem
                         icon={<LucideHome/>}
@@ -37,12 +40,14 @@ export default function Sidebar({className}: {className?: string}) {
                         label="Notification"
                         link="/notification"
                     />
-                    <div className="flex items-center gap-3 mt-3 cursor-pointer">
-                        <Avatar>
-                            <AvatarFallback>BN</AvatarFallback>
-                        </Avatar>
-                        <TypographyP className="!m-0 !text-sm">Rithy Bondeth</TypographyP>
-                    </div>
+                    <Link href="/profile">
+                        <div className="flex items-center gap-3 mt-3 cursor-pointer">
+                            <Avatar>
+                                <AvatarFallback>BN</AvatarFallback>
+                            </Avatar>
+                            <TypographyP className="!m-0 !text-sm">Rithy Bondeth</TypographyP>
+                        </div>
+                    </Link>
                 </div>
             </div>
             <div className="w-full flex justify-center">
