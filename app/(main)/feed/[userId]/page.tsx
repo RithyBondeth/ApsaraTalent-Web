714bd5a688273da6bf7281ded2077dc2fa34ce64
Rithy Bondeth <rithybondeth999@gmail.com>
2025-02-23 17:00:04 +0700

    handle layout for user detail page

--- app/(main)/feed/[userId]/page.tsx
+++ app/(main)/feed/[userId]/page.tsx
@@ -1,5 +1,51 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import Label from "@/components/utils/label";
+import { TypographyH4 } from "@/components/utils/typography/typography-h4";
+import { TypographyMuted } from "@/components/utils/typography/typography-muted";
+import { LucideAtSign, LucideMail, LucideMapPin, LucidePhone, LucideUser } from "lucide-react";
+
 export default function UserDetailPage() {
     return (
-        <div>User Detail Page</div>
+        <div>
+            <div className="w-full flex items-stretch justify-between border border-muted py-5 px-10">
+                <div className="flex flex-col items-center gap-5">
+                    <Avatar className="size-40">
+                        <AvatarFallback>BN</AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col items-center gap-1">
+                        <TypographyH4>Hem RithyBondeth</TypographyH4>
+                        <TypographyMuted>Software Engineer</TypographyMuted>
+                    </div>
+                </div>
+                <div className="flex flex-col items-start gap-5">
+                    <div className="flex flex-col items-start gap-2">
+                        <TypographyMuted>Firstname</TypographyMuted>
+                        <Label icon={<LucideUser className="!size-5"/>} text="Hem"/>        
+                    </div>
+                   <div className="flex flex-col items-start gap-2">
+                        <TypographyMuted>Lastname</TypographyMuted>
+                        <Label icon={<LucideUser className="!size-5"/>} text="RithyBondeth" />        
+                    </div>
+                    <div className="flex flex-col items-start gap-2">
+                        <TypographyMuted>Username</TypographyMuted>
+                        <Label icon={<LucideAtSign className="!size-5"/>} text="_benx123_" />        
+                    </div>
+                </div>
+                <div className="flex flex-col items-start gap-5">
+                    <div className="flex flex-col items-start gap-2">
+                        <TypographyMuted>Email</TypographyMuted>
+                        <Label icon={<LucideMail className="!size-5"/>} text="rithybondeth@gmail.com" />        
+                    </div>
+                    <div className="flex flex-col items-start gap-2">
+                        <TypographyMuted>Phone</TypographyMuted>
+                        <Label icon={<LucidePhone className="!size-5"/>} text="085872582" />
+                    </div>
+                    <div className="flex flex-col items-start gap-2">
+                        <TypographyMuted>Address</TypographyMuted>
+                        <Label icon={<LucideMapPin className="!size-5"/>} text="Phnom Penh, Cambodia" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,28 @@
+"use client"
+
 import LeftSidebar from "@/components/sidebar/left-sidebar";
 import RightSidebar from "@/components/sidebar/right-sidebar";
-
+import { Button } from "@/components/ui/button";
+import { LucideArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function MainLayout({children}: {children: React.ReactNode}) {
+    const pathname = usePathname();
+
+    // Exclude dynamic feed pages but include /feed
+    if (pathname.startsWith("/feed/") && pathname !== "/feed") {
+        return (
+            <div className="relative p-5"> 
+                <Link href="/feed">
+                    <Button variant={'outline'} size="icon" className="absolute top-5 left-5">
+                        <LucideArrowLeft/>
+                    </Button>
+                </Link>
+                <div className="container mx-auto">{children}</div>
+            </div>     
+        ); // No layout for /feed/[userId]
+    }
+
     return (
         <div className="flex justify-between items-start p-5"> 
             <LeftSidebar className="h-[calc(100vh-40px)] w-[20%] rounded-lg"/>
