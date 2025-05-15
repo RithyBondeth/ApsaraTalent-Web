2ff7b6e33fe7d6a74f63dc759e30e3529c8e3aaf
Rithy Bondeth <rithybondeth999@gmail.com>
2025-05-15 15:07:35 +0700

    done login api integration

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,14 +1,26 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import SocialButton from "@/components/utils/buttons/social-button";
-import { TypographyH2 } from "@/components/utils/typography/typography-h2"
-import { TypographyMuted } from "@/components/utils/typography/typography-muted"
+import { TypographyH2 } from "@/components/utils/typography/typography-h2";
 import { TypographySmall } from "@/components/utils/typography/typography-small";
-import { googleIcon, facebookIcon, linkedinIcon, githubIcon } from '@/utils/constants/asset.constant';
-import { LucideEye, LucideEyeClosed, LucideInfo, LucideLockKeyhole, LucideMail, LucidePhone, LucideUser, LucideUser2 } from "lucide-react";
+import {
+  googleIcon,
+  facebookIcon,
+  linkedinIcon,
+  githubIcon,
+} from "@/utils/constants/asset.constant";
+import {
+  LucideCheck,
+  LucideEye,
+  LucideEyeClosed,
+  LucideInfo,
+  LucideLockKeyhole,
+  LucideMail,
+  LucidePhone,
+} from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -19,26 +31,30 @@ import Image from "next/image";
 import { useTheme } from "next-themes";
 import { useForm } from "react-hook-form";
 import { loginSchema, TLoginForm } from "./validation";
-import { zodResolver } from '@hookform/resolvers/zod';
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useLoginStore } from "@/stores/apis/auth/login.store";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
 import { ClipLoader } from "react-spinners";
+import { TypographyMuted } from "@/components/utils/typography/typography-muted";
 
 function LoginPage() {
   const [passwordVisibility, setPasswordVisibility] = useState<boolean>(false);
   const router = useRouter();
-
   const { resolvedTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
   const { toast } = useToast();
 
-  const { handleSubmit, register, formState: { errors }, reset } = useForm<TLoginForm>({
-    resolver: zodResolver(loginSchema)
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm<TLoginForm>({
+    resolver: zodResolver(loginSchema),
   });
 
-  const { accessToken, refreshToken, message, login, error, loading } = useLoginStore(); 
+  const { accessToken, refreshToken, message, login, error, loading } =
+    useLoginStore();
 
   const onSubmit = async (data: TLoginForm) => {
     await login(data.email, data.password);
@@ -47,113 +63,182 @@ function LoginPage() {
       accessToken: accessToken,
       refreshToken: refreshToken,
     });
-  }
-  
-  useEffect(() => {
-    if(accessToken && refreshToken) router.push("/feed");
-    
-    if(loading) toast({
-      description: <div className="flex items-center gap-2">
-        <ClipLoader/>
-        <TypographySmall className="font-semibold">Logging in...</TypographySmall>
-      </div>
-    });
+  };
 
-    if(error) toast({
-      variant: "destructive",
-      description: <div className="flex flex-row items-center gap-2">
-        <LucideInfo/>
-        <TypographySmall className="font-semibold">{message}</TypographySmall>
-      </div>,
-      action: <ToastAction altText="Try again" onClick={() => reset()}>Retry</ToastAction>,
-    })
-    setMounted(true);
-  }, [accessToken, refreshToken, error, message]);
+  useEffect(() => {
+    if (accessToken && refreshToken) {
+      toast({
+        description: <div className="flex items-center gap-2"> 
+          <LucideCheck/>
+          <TypographySmall className="font-medium">Logged in successfully</TypographySmall>
+        </div>,
+        duration: 2000,
+      });
   
+      const timeout = window.setTimeout(() => {
+        router.push("/feed");
+      }, 1000); // 2-second delay
+  
+      return () => clearTimeout(timeout); // Cleanup
+    }
+
+    if (loading)
+      toast({
+        description: (
+          <div className="flex items-center gap-2">
+            <ClipLoader />
+            <TypographySmall className="font-medium">
+              Logging in...
+            </TypographySmall>
+          </div>
+        ),
+      });
+
+    if (error)
+      toast({
+        variant: "destructive",
+        description: (
+          <div className="flex flex-row items-center gap-2">
+            <LucideInfo/>
+            <TypographySmall className="font-medium leading-normal">
+              {message}
+            </TypographySmall>
+          </div>
+        ),
+        action: (
+          <ToastAction altText="Try again" onClick={() => reset()}>
+            Retry
+          </ToastAction>
+        ),
+      });
+  }, [accessToken, refreshToken, error, message]);
+
   // Determine which image to display (avoid SSR issues)
-  const currentTheme = mounted ? resolvedTheme : "light";
+  const currentTheme = resolvedTheme || "light";
   const loginImage = currentTheme === "dark" ? loginWhiteSvg : loginBlackSvg;
 
   return (
     <div className="h-screen w-screen flex justify-between items-stretch tablet-lg:flex-col tablet-lg:[&>div]:w-full">
       <div className="h-screen w-1/2 flex justify-center items-center bg-primary-foreground tablet-lg:h-fit">
-         <div className="size-[70%] flex flex-col items-start justify-center gap-3 tablet-md:w-[85%] tablet-md:py-10">
-            {/* Title Section */}
-            <div>
-              <LogoComponent/>
-              <TypographyH2 className="phone-xl:text-2xl">Log in to your Account</TypographyH2>
-              <TypographyMuted className="text-md phone-xl:text-sm">Welcome to Apsara Talent! Select method to log in</TypographyMuted>
+        <div className="size-[70%] flex flex-col items-start justify-center gap-3 tablet-md:w-[85%] tablet-md:py-10">
+          {/* Title Section */}
+          <div>
+            <LogoComponent />
+            <TypographyH2 className="phone-xl:text-2xl">
+              Log in to your Account
+            </TypographyH2>
+            <TypographyMuted className="text-md phone-xl:text-sm">
+              Welcome to Apsara Talent! Select method to log in
+            </TypographyMuted>
+          </div>
+          {/* End Title Section */}
+          {/* Social Button Login Section */}
+          <div className="w-full flex flex-col gap-3">
+            <div className="flex justify-between items-center gap-3">
+              <SocialButton
+                image={googleIcon}
+                label="Google"
+                variant="outline"
+                className="w-1/2"
+              />
+              <SocialButton
+                image={facebookIcon}
+                label="Google"
+                variant="outline"
+                className="w-1/2"
+              />
             </div>
-            {/* End Title Section */}
-            {/* Social Button Login Section */}
-            <div className="w-full flex flex-col gap-3">
-              <div className="flex justify-between items-center gap-3">
-                <SocialButton image={googleIcon} label="Google" variant="outline" className="w-1/2"/>
-                <SocialButton image={facebookIcon} label="Google" variant="outline" className="w-1/2"/>
-              </div>
-              <div className="flex justify-between items-center gap-3">
-                <SocialButton image={linkedinIcon} label="LinkedIn" variant="outline" className="w-1/2"/>
-                <SocialButton image={githubIcon} label="Github" variant="outline" className="w-1/2"/>
-              </div>
-              <Button variant="outline" onClick={() => router.push('login/phone-number')}>
-                <LucidePhone/>
-                Phone Number
-              </Button>
+            <div className="flex justify-between items-center gap-3">
+              <SocialButton
+                image={linkedinIcon}
+                label="LinkedIn"
+                variant="outline"
+                className="w-1/2"
+              />
+              <SocialButton
+                image={githubIcon}
+                label="Github"
+                variant="outline"
+                className="w-1/2"
+              />
             </div>
-            {/* End Social Button Login Section */}
-            {/* Divider Section */}
-            <div className="w-full flex justify-between items-center">
-              <Separator className="w-1/3"/>
-              <TypographyMuted className="text-xs">or continue with email</TypographyMuted>
-              <Separator className="w-1/3"/>
+            <Button
+              variant="outline"
+              onClick={() => router.push("login/phone-number")}
+            >
+              <LucidePhone />
+              Phone Number
+            </Button>
+          </div>
+          {/* End Social Button Login Section */}
+          {/* Divider Section */}
+          <div className="w-full flex justify-between items-center">
+            <Separator className="w-1/3" />
+            <TypographyMuted className="text-xs">
+              or continue with email
+            </TypographyMuted>
+            <Separator className="w-1/3" />
+          </div>
+          {/* End Divider Section */}
+          {/* Login Form Section Section */}
+          <form
+            className="w-full flex flex-col items-stretch gap-5"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col gap-5">
+              <Input
+                prefix={<LucideMail />}
+                placeholder="Email"
+                type="email"
+                {...register("email")}
+                validationMessage={errors.email?.message}
+              />
+              <Input
+                prefix={<LucideLockKeyhole />}
+                suffix={
+                  passwordVisibility ? (
+                    <LucideEyeClosed
+                      onClick={() => setPasswordVisibility(false)}
+                    />
+                  ) : (
+                    <LucideEye onClick={() => setPasswordVisibility(true)} />
+                  )
+                }
+                placeholder="Password"
+                type={passwordVisibility ? "text" : "password"}
+                {...register("password")}
+                validationMessage={errors.password?.message}
+              />
             </div>
-            {/* End Divider Section */}
-            {/* Login Form Section Section */}
-            <form  className="w-full flex flex-col items-stretch gap-5" onSubmit={handleSubmit(onSubmit)}>
-              <div className="flex flex-col gap-5">
-                <Input
-                  prefix={<LucideMail/>}
-                  placeholder="Email"
-                  type="email"
-                  {...register('email')}
-                  validationMessage={errors.email?.message}
-                />
-                <Input
-                  prefix={<LucideLockKeyhole/>}
-                  suffix={passwordVisibility 
-                    ? <LucideEyeClosed onClick={() => setPasswordVisibility(false)}/> 
-                    : <LucideEye onClick={() => setPasswordVisibility(true)}/>}
-                  placeholder="Password"
-                  type={passwordVisibility ? "text" : "password"}
-                  {...register('password')}
-                  validationMessage={errors.password?.message}
-                />
+            <div className="flex justify-between items-center mb-5">
+              <div className="flex items-center gap-1">
+                <Checkbox name="remember" />
+                <TypographyMuted className="text-xs">
+                  Remember me
+                </TypographyMuted>
               </div>
-              <div className="flex justify-between items-center mb-5">
-                  <div className="flex items-center gap-1">
-                      <Checkbox name="remember"/>
-                      <TypographyMuted className="text-xs">Remember me</TypographyMuted>
-                  </div>
-                  <TypographySmall className="text-xs cursor-pointer hover:text-muted-foreground">
-                    <Link href="/forgot-password">Forgot Password?</Link>
-                  </TypographySmall>
-              </div>
-              <Button type="submit" disabled={loading}>Login</Button>
-              <div className="flex items-center gap-2 mx-auto">
-                  <TypographyMuted>Do not have account?</TypographyMuted>
-                  <Link href='/signup'>
-                    <TypographySmall className="text-xs cursor-pointer hover:text-muted-foreground">Create account</TypographySmall>
-                  </Link>
-              </div>
-            </form>
-            {/* End Login Form Section Section */}
-         </div>
+              <TypographySmall className="text-xs cursor-pointer hover:text-muted-foreground">
+                <Link href="/forgot-password">Forgot Password?</Link>
+              </TypographySmall>
+            </div>
+            <Button type="submit" disabled={loading}>Login</Button>
+            <div className="flex items-center gap-2 mx-auto">
+              <TypographyMuted>Do not have account?</TypographyMuted>
+              <Link href="/signup">
+                <TypographySmall className="text-xs cursor-pointer hover:text-muted-foreground">
+                  Create account
+                </TypographySmall>
+              </Link>
+            </div>
+          </form>
+          {/* End Login Form Section Section */}
+        </div>
       </div>
       <div className="w-1/2 flex justify-center items-center bg-primary tablet-lg:p-10">
-        <Image src={loginImage} alt="login" height={undefined} width={600}/>
+        <Image src={loginImage} alt="login" height={undefined} width={600} />
       </div>
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
