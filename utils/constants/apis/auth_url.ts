87c3423a7e63c05473b1eeaae4e9e0c060a2ba32
Rithy Bondeth <rithybondeth999@gmail.com>
2025-12-16 11:03:51 +0700

    add cmpUnfavoriteEmo and empUnfavoriteCmp api constants

--- utils/constants/apis/auth_url.ts
+++ utils/constants/apis/auth_url.ts
@@ -4,17 +4,21 @@ export const API_AUTH_URL = API_BASE_URL + "/auth";
 export const API_AUTH_LOGIN_URL = API_AUTH_URL + "/login";
 export const API_AUTH_LOGOUT_URL = API_AUTH_URL + "/logout";
 export const API_AUTH_SIGNUP_URL = {
-    COMPANY: API_AUTH_URL + "/register-company",
-    EMPLOYEE: API_AUTH_URL + "/register-employee"
-}
+  COMPANY: API_AUTH_URL + "/register-company",
+  EMPLOYEE: API_AUTH_URL + "/register-employee",
+};
 export const API_AUTH_FORGOT_PASSWORD_URL = API_AUTH_URL + "/forgot-password";
-export const API_AUTH_RESET_PASSWORD_URL = (token: string) => `${API_AUTH_URL}/reset-password/${token}`;
-export const API_AUTH_VERIFY_EMAIL_URL = (emailVerificationToken: string) => `${API_AUTH_URL}/verify-email/${emailVerificationToken}`;
+export const API_AUTH_RESET_PASSWORD_URL = (token: string) =>
+  `${API_AUTH_URL}/reset-password/${token}`;
+export const API_AUTH_VERIFY_EMAIL_URL = (emailVerificationToken: string) =>
+  `${API_AUTH_URL}/verify-email/${emailVerificationToken}`;
 
 export const API_AUTH_VERIFY_OTP_URL = API_AUTH_URL + "/verify-otp";
 export const API_AUTH_LOGIN_OTP_URL = API_AUTH_URL + "/login-otp";
 
 export const API_AUTH_SOCIAL_GOOGLE_URL = API_BASE_URL + "/social/google/login";
-export const API_AUTH_SOCIAL_LINKEDIN_URL = API_BASE_URL + "/social/linkedin/login";
+export const API_AUTH_SOCIAL_LINKEDIN_URL =
+  API_BASE_URL + "/social/linkedin/login";
 export const API_AUTH_SOCIAL_GITHUB_URL = API_BASE_URL + "/social/github/login";
-export const API_AUTH_SOCIAL_FACEBOOK_URL = API_BASE_URL + "/social/facebook/login";
+export const API_AUTH_SOCIAL_FACEBOOK_URL =
+  API_BASE_URL + "/social/facebook/login";
--- utils/constants/apis/favorite_url.ts
+++ utils/constants/apis/favorite_url.ts
@@ -5,10 +5,24 @@ export const API_EMPLOYEE_FAVORITE_COMPANY_URL = (
   cmpId: string
 ) => `${API_BASE_URL}/user/employee/${empId}/favorite/company/${cmpId}`;
 
+export const API_EMPLOYEE_UNFAVORITE_COMPANY_URL = (
+  empId: string,
+  cmpId: string,
+  favoriteId: string
+) =>
+  `${API_BASE_URL}/user/employee/${empId}/unfavorite/${favoriteId}/company/${cmpId}`;
+
 export const API_COMPANY_FAVORITE_EMPLOYEE_URL = (
   cmpId: string,
   empId: string
 ) => `${API_BASE_URL}/user/company/${cmpId}/favorite/employee/${empId}`;
+
+export const API_COMPANY_UNFAVORITE_EMPLOYEE_URL = (
+  cmpId: string,
+  empId: string,
+  favoriteId: string
+) =>
+  `${API_BASE_URL}/user/company/${cmpId}/unfavorite/${favoriteId}/employee/${empId}`;
 
 export const API_FIND_ALL_EMPLOYEE_FAVORITES = (empId: string) =>
   `${API_BASE_URL}/user/employee/all-favorites/${empId}`;
