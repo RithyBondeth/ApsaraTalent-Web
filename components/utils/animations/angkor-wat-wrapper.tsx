3e80e59fcacde7cdf3653cb8360555f8088f4574
Rithy Bondeth <rithybondeth999@gmail.com>
2026-04-30 12:42:09 +0700

    perf: lazy load AngkorWatScene with dynamic import to disable SSR

--- components/utils/animations/angkor-wat-wrapper.tsx
+++ components/utils/animations/angkor-wat-wrapper.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import AngkorWatScene from "@/components/utils/animations/angkor-wat-scene";
+import dynamic from "next/dynamic";
+
+const AngkorWatScene = dynamic(
+  () => import("@/components/utils/animations/angkor-wat-scene"),
+  {
+    ssr: false,
+  },
+);
 
 export function AngkorWatWrapper() {
   /* --------------------------------- Render UI -------------------------------- */
