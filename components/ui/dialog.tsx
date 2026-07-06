45439f54afd89a771385570673f8a8a3d2578461
RITHY Bondeth <rithybondeth999@gmail.com>
2026-07-06 10:27:58 +0700

    style: update dialog component padding to py-4 px-3

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -36,7 +36,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-0 right-0 top-1/2 z-50 mx-auto flex w-[94vw] max-w-lg -translate-y-1/2 flex-col gap-0 overflow-hidden border bg-background p-0 shadow-lg duration-200 focus-visible:outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] max-h-[90vh] sm:rounded-xl isolate",
+        "fixed left-0 right-0 top-1/2 z-50 mx-auto flex w-[94vw] max-w-lg -translate-y-1/2 flex-col gap-0 overflow-hidden border bg-background py-4 px-3 shadow-lg duration-200 focus-visible:outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] max-h-[90vh] sm:rounded-xl isolate",
         className,
       )}
       {...props}
